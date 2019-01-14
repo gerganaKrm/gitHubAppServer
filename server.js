583d8fe0ae0a9eb10ec149f8f45fe085749fd815
Gergana Kremenska <gergana.kremenska@sap.com>
2019-01-14 21:34:14 +0200

    clean up

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express   = require('express'),
 	cors = require('cors'),
 	bodyParser = require('body-parser'),
 	request = require("request"),
+	// Add cache
 	chache = require("node-cache");
 
 var app = express();
 
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// TBD pinned repo - api?
+//Commits, contributors... - count in table?
 var pinnedRepos = [
 	{
 		name: "clarity",
@@ -58,30 +61,27 @@ var pinnedRepos = [
 		license: "Other"
 	}
 ];
+
 app.post('/login', function (req, res) {
-	var userData = req.body,
-	userExist = findUser(userData);
-	console.log("test: " + userExist);
+	var userExist = findUser(req.body);
 	res.send(userExist)
 });
+
 app.get('/repos', function (req, res) {
 	res.send(pinnedRepos);
 });
 
-
 app.get('/repoDetails', function (req, res) {
-	var url = "https://api.github.com/repos/vmware/" + req.param("repoName") + '/commits';
-	var options = {
-		url: url,
-		method: 'GET',
-		headers: {'user-agent': 'node.js'}
-	};
-	console.log(req.param("repoName"));
+		var options = {
+			url: "https://api.github.com/repos/vmware/" + req.param("repoName") + '/commits',
+			method: 'GET',
+			headers: {'user-agent': 'node.js'}
+		};
+
 	request( options , (error, response, body) => {
 		if(error) {
-			return console.log(error);
+			res.send(error);
 		}
-		console.log(url);
 		res.send(body);
 	});
 });
@@ -92,29 +92,26 @@ app.get('/repoDescription', function (req, res) {
 		method: 'GET',
 		headers: {'user-agent': 'node.js'}
 	};
-	console.log(req.param("repoName"));
 	request( options , (error, response, body) => {
 		if(error) {
-			return console.log(error);
+			res.send(error);
 		}
 		res.send(body);
 	});
 });
 
 app.get('/commitPatch', function (req, res) {
-	var url = "https://api.github.com/repos/vmware/" + req.param("repoName") + "/commits/" + req.param("sha");
 	var options = {
-		url: url,
+		url: "https://api.github.com/repos/vmware/" + req.param("repoName") + "/commits/" + req.param("sha"),
 		method: 'GET',
 		headers: {
 			'user-agent': 'node.js',
 			'Content-Type': 'application/vnd.github.VERSION.patch'
 		}
 	};
-	console.log(url);
 	request( options , (error, response, body) => {
 		if(error) {
-			return console.log(error);
+			res.send(body);
 		}
 		res.send(body);
 	});
@@ -126,14 +123,16 @@ function findUser(user) {
 		username: "some",
 		password: "123"
 	};
-	console.log(user);
+
 	if (testUser.username === user.user && testUser.password === user.password) {
-		return {res: "true"};
+
+		return {res: true};
 	} else {
-		return false;
+
+		return {res: false};
 	}
 }
 
 app.listen( 3000, function(){
-	console.log("listening on port 300")
-}) ;
+	console.log("listening on port 3000");
+});
